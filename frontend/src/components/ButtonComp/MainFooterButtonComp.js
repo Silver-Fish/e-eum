431df69dc04c8c6c65013da70aec9f28448218b1
shindonghyun <donghyun5349@naver.com>
2021-03-10 02:57:57 +0900

    feat / front / 메인페이지생성 / S04P22B104-48

---
+++ frontend/src/components/ButtonComp/MainFooterButtonComp.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './MainFooterButtonComp.module.css'
+import { useHistory } from "react-router-dom";
+
+const MainFooterButton = (props) => {
+  const history = useHistory();
+  const buttonImg = props.buttonImg
+  const textValue = props.textValue
+  
+  const handleClick = (e) => {    
+    history.push(props.handleClickPath) 
+  }
+
+
+  return(  
+    <button className={styles.button} onClick={handleClick}>
+      <img className={styles.buttonimage} src={buttonImg} alt=""/>
+      {textValue}
+    </button>
+  )
+}
+
+
+export default MainFooterButton
