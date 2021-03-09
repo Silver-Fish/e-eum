24495205db08f7499e56cc90bc56f44aaa37ab20
juheegg <eespacio95@gmail.com>
2021-03-09 10:05:54 +0900

    init backend

---
+++ backend/src/main/java/com/ssafy/eeum/EeumApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.eeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeumApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/eeum/EeumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.eeum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
