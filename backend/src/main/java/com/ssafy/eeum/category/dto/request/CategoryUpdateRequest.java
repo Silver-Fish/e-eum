887e3cb7b6ffeaf89fe4c893ffe41f7d445d1e2c
차수연 <pepprbell@gmail.com>
2021-03-17 17:10:48 +0900

    feat / back / 카테고리 수정 DTO 개발 / S04P22B104-171

---
+++ backend/src/main/java/com/ssafy/eeum/category/dto/request/CategoryUpdateRequest.java
@@ -0,0 +1,36 @@
+package com.ssafy.eeum.category.dto.request;
+
+import com.ssafy.eeum.category.domain.Category;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+
+@NoArgsConstructor
+@Getter
+@ApiModel(description = "카테고리 수정 모델")
+public class CategoryUpdateRequest {
+
+    @ApiModelProperty(value = "단어")
+    @NotBlank
+    @Length(max = 10, message = "카테고리 이름의 총 길이는 10자 입니다.")
+    private String word;
+
+    @ApiModelProperty(value = "이미지")
+    private String categoryImageUrl;
+
+    public CategoryUpdateRequest (@NotBlank @Length(max = 10, message = "카테고리 이름의 총 길이는 10자 입니다.") String word, String categoryImageUrl) {
+        this.word = word;
+        this.categoryImageUrl = categoryImageUrl;
+    }
+
+    public Category toCategory(){
+        return Category.builder()
+                .word(word)
+                .categoryImageUrl(categoryImageUrl)
+                .build();
+    }
+}
