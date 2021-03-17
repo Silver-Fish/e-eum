f138a66533d0e97a8517c190d0255c50650c7383
차수연 <pepprbell@gmail.com>
2021-03-17 16:37:13 +0900

    feat / back / 카테고리 Entity 추가 / S04P22B104-180

---
+++ backend/src/main/java/com/ssafy/eeum/category/domain/Category.java
@@ -0,0 +1,49 @@
+package com.ssafy.eeum.category.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="category")
+public class Category {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+//    @ManyToOne  :  유저
+//    @JoinColumn(name = "user_no")
+//    private Users user;
+
+    private String word;
+
+//    @ColumnDefault("") : 이미지 디폴트값 설정용 어노테이션
+    private String categoryImageUrl;
+
+    @Temporal(TemporalType.DATE)
+    @CreationTimestamp
+    private Date createDate;
+//
+    @Temporal(TemporalType.DATE)
+    @UpdateTimestamp
+    private Date updateDate;
+
+    @Builder
+    public Category(String word, String categoryImageUrl) {
+        this.word = word;
+        this.categoryImageUrl = categoryImageUrl;
+    }
+
+    public Category update(Category category) {
+        this.word = category.word;
+        this.categoryImageUrl = category.categoryImageUrl;
+
+        return this;
+    }
+}
