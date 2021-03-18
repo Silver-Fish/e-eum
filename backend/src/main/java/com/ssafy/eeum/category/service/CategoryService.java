04e5a26fc6e540ec085d4b530568b1c4f242abb4
차수연 <pepprbell@gmail.com>
2021-03-18 16:05:32 +0900

    feat / back / 카테고리 수정 service 추가 / S04P22B104-173

--- backend/src/main/java/com/ssafy/eeum/category/service/CategoryService.java
+++ backend/src/main/java/com/ssafy/eeum/category/service/CategoryService.java
@@ -2,6 +2,9 @@ package com.ssafy.eeum.category.service;
 
 import com.ssafy.eeum.category.domain.Category;
 import com.ssafy.eeum.category.dto.request.CategoryRequest;
+import com.ssafy.eeum.category.dto.request.CategoryUpdateRequest;
+import com.ssafy.eeum.category.dto.response.CategoryResponse;
+import com.ssafy.eeum.category.exception.CategoryNotFound;
 import com.ssafy.eeum.category.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +25,20 @@ public class CategoryService {
         categoryRepository.save(category);
 
         return category.getId();
+    }
+
+    // 카테고리 수정
+    @Transactional
+    public CategoryResponse updateCategory(Long id, CategoryUpdateRequest categoryUpdateRequest) {
+        Category category = findById(id);
+        Category requestCategory = categoryUpdateRequest.toCategory();
+
+        Category updatedCategory = category.update(requestCategory);
+        return CategoryResponse.of(updatedCategory);
+    }
+
+    public Category findById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new CategoryNotFound());
     }
 }
