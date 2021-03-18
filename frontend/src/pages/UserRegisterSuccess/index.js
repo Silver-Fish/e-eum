5e689b030c16815f3f7222f6f48573cbd50a2741
AgFe <dbsdmscjf304@naver.com>
2021-03-19 02:36:28 +0900

    feat / fornt / SignUpSuccess / S04P22B104-88

--- frontend/src/pages/UserRegisterSuccess/index.js
+++ frontend/src/pages/UserRegisterSuccess/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import ImgboxTitle from '../../components/Image/ImgboxTitle';
+import Imgbox from '../../components/Image/Imgbox';
 import UserButtonComp from '../../components/ButtonComp/UserButtonComp';
 
 const UserRegisterSuccess = () => {
-
-  
-    return (
-        <div>
-            <ImgboxTitle src = "/images/userRegisterSuccess.PNG" /><br/>
-            <UserButtonComp textValue ="로그인하러가기" handleClick ="login"></UserButtonComp>
-        </div>
-    );
+  const history = useHistory();
+  useEffect(() => {
+    setTimeout(function () {
+      history.push('./');
+    }, 3000);
+  });
+  return (
+    <div>
+      <ImgboxTitle src="/images/userRegisterSuccess.PNG" />
+      <Imgbox src="/images/cat.PNG" />
+      <UserButtonComp textValue="로그인 하러가기" handleClick="login"></UserButtonComp>
+    </div>
+  );
 };
 
 export default UserRegisterSuccess;
