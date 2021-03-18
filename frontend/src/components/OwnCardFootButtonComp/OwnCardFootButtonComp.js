e82e9da4ea9b5bc4f7397b582ab00d00503642b3
JaeHyeok <roywogur@naver.com>
2021-03-18 13:42:31 +0900

    feat/front/owncarComp S04P22B104-85

---
+++ frontend/src/components/OwnCardFootButtonComp/OwnCardFootButtonComp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OwnCardFootButtonComp = () => {
+  return (
+    <div>
+      <button>추가</button>
+      <button>수정</button>
+    </div>
+  );
+};
+
+export default OwnCardFootButtonComp;
