998abb717ccbe9f25fe641ed4ec0a17e91d627b2
juheegg <eespacio95@gmail.com>
2021-03-18 20:35:15 +0900

    fix / back / cors header 문제 해결

--- backend/src/main/java/com/ssafy/eeum/common/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/eeum/common/config/SecurityConfig.java
@@ -21,6 +21,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 /**
@@ -74,6 +75,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedOrigins(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setExposedHeaders(Arrays.asList("Authorization"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
