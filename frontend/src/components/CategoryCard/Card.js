d95b0b6d4c274707cc3717fbf278bf3586b35cd6
shindonghyun <donghyun5349@naver.com>
2021-03-18 21:57:18 +0900

    feat / front / category 추가 S04P22B104-105

--- frontend/src/components/CategoryCard/Card.js
+++ frontend/src/components/CategoryCard/Card.js
@@ -5,13 +5,18 @@ import styles from './CategoryCard.module.css';
 
   
 const Card = (props) => {
+  
   const textValue = props.textValue 
-  const handleClick = (e) => {    
-    props.categoryState(false)
+  const cardUrl = props.cardUrl
+  const cardButtonClick = (e) => {    
+    props.cardClick({
+      cardName: {textValue}, 
+      cardUrl: {cardUrl}
+    })
   }
   return(
-    <button className={styles.card} onClick={handleClick}>
-      <img className={styles.card_image} src='/images/user.png' alt=""/>
+    <button className={styles.card} onClick={cardButtonClick}>
+      <img className={styles.card_image} src={cardUrl} alt=""/>
         {textValue}
     </button>
   )
--- frontend/src/components/CategoryCard/CategoryCard.js
+++ frontend/src/components/CategoryCard/CategoryCard.js
@@ -1,21 +1,55 @@
 import React from 'react';
+import { useState } from 'react';
 import styles from './CategoryCard.module.css';
-import { useHistory } from "react-router-dom";
 
 
 
   
 const CategoryCard = (props) => {
-  const textValue = props.textValue
-  const handleClick = (e) => {    
+  const [isCategoryCardEdit, setCategoryCardEdit] = useState(false)
+  const isCategoryEdit = props['isCategoryEdit']
+  const categoryName = props.textValue
+  const imgUrl = props.categoryUrl
+
+  const categoryCardClick = (e) => {    
+    
+    props.categoryTitle({categoryName}['categoryName'])
     props.categoryState(false)
-    console.log('백이랑 통신이 필요함')
+    console.log('백이랑 통신이 필요함');
   }
+  
+  const categoryDeleteClick = (e) => {    
+    e.stopPropagation();
+    console.log('서버와 삭제 통신해야함')
+  }
+  const categoryCardEditClick = (e) => {
+    
+    props.categoryCardEdit({state:!isCategoryCardEdit, url:imgUrl, name: {categoryName}['categoryName']})
+    
+    
+    // console.log(URL.createOb.buttonjectURL(e.target.src))
+  }
+  
   return(
-    <button className={styles.card} onClick={handleClick}>
-      <img className={styles.card_image} src='/images/user.png' alt=""/>
-        {textValue}
-    </button>
+    <>
+      { isCategoryEdit === false
+        ?
+        <button className={styles.card} onClick={categoryCardClick}>
+          <img className={styles.card_image} src={imgUrl} alt=""/>
+            {categoryName}
+        </button>
+        :
+        <>       
+          <button className={styles.card_is_edit} onClick={categoryCardEditClick}>
+            <div className={styles.card_del_box} >
+              <img src="/images/minus.png" alt="" onClick={categoryDeleteClick}/>
+            </div>
+            <img className={styles.card_image} src={imgUrl} alt=""/>
+            {categoryName}
+          </button>
+        </>
+      }
+    </>
   )
 }
 
