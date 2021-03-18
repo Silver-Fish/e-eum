347370d5024a0b72906d5061dcea0aa8bc47ddd4
차수연 <pepprbell@gmail.com>
2021-03-19 02:19:28 +0900

    fix / back / id 로직 수정 / S04P22B104-180

--- backend/src/main/java/com/ssafy/eeum/category/domain/Category.java
+++ backend/src/main/java/com/ssafy/eeum/category/domain/Category.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 //@Table(name="category")
 public class Category {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 //    @ManyToOne  :  유저
