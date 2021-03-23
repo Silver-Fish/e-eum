b1faa5e43f126f3f898fc951af4de786a2df86e9
AgFe <dbsdmscjf304@naver.com>
2021-03-23 18:47:46 +0900

    feat / front / 본인확인Back연결/S04P23B104-12

--- frontend/src/pages/Confirm/index.js
+++ frontend/src/pages/Confirm/index.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react';
 import ImgboxTitle from '../../components/Image/ImgboxTitle';
 import { useHistory } from 'react-router-dom';
-
+import axios from 'axios';
 import styles from './index.module.css';
 
 
@@ -16,22 +16,23 @@ const Confirm = () => {
       };
     const onSubmitHandler = (e) => {
         e.preventDefault();
-        // axios
-        //   .get('http://localhost:8080/api/accouts/find?email=' + email + '&name=' + name)
-        //   .then((res) => {
-        //     if (res.data) {
-        //       setModal(true);
-        //     } else {
-        //       alert('이메일 또는 이름 오류');
-        //     }
-        //   })
-        //   .catch((err) => {
-        //     alert('오류발생');
-        //   });
+        axios
+          .get('http://localhost:8080/api/accouts/confirm?password=' + password,{
+              headers:{
+                  Authorization: sessionStorage.getItem('jwt'),
+              },
+          })
+          .then((res) => {
+            if (res.data) {
+              history.push('./userDelete');
+            } else {
+              alert('비번 틀림!');
+            }
+          })
+          .catch((err) => {
+            alert('오류발생');
+          });
       }; 
-
-
- 
 
     return (
         <div>
