96011992fabb5102727fbdd12074fa5dfef5ae03
차수연 <pepprbell@gmail.com>
2021-03-25 22:53:19 +0900

    fix / back / 이미지 삽입 위한 service 수정 / S04P23B104-98

--- backend/src/main/java/com/ssafy/eeum/category/service/CategoryService.java
+++ backend/src/main/java/com/ssafy/eeum/category/service/CategoryService.java
@@ -2,6 +2,8 @@ package com.ssafy.eeum.category.service;
 
 import com.ssafy.eeum.account.domain.Account;
 import com.ssafy.eeum.account.repository.AccountRepository;
+import com.ssafy.eeum.card.domain.Card;
+import com.ssafy.eeum.category.domain.AccountCategory;
 import com.ssafy.eeum.category.domain.Category;
 import com.ssafy.eeum.category.dto.request.CategoryUpdateRequest;
 import com.ssafy.eeum.category.dto.response.CategoriesResponse;
@@ -10,6 +12,7 @@ import com.ssafy.eeum.category.repository.CategoryRepository;
 import com.ssafy.eeum.common.exception.ErrorCode;
 import com.ssafy.eeum.common.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -19,13 +22,15 @@ import javax.transaction.Transactional;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
 
-    @Value("${file.path}")
+    @Value("D:/")
     private String filePath;
 
     private final CategoryRepository categoryRepository;
@@ -33,20 +38,20 @@ public class CategoryService {
 
     // 카테고리 등록
     @Transactional
-    public Long save(String email, String word, MultipartFile image) throws IOException {
-        Account account = accountRepository.findByEmail(email)
-                .orElseThrow(() -> {return new NotFoundException(ErrorCode.USER_NOT_FOUND);});
-
+    public Long save(Account account, String word, MultipartFile image) throws Exception {
         Category category = Category.builder().word(word).build();
         categoryRepository.save(category);
-        account.addCategory(category);
 
-        String imageUrl = "category/" + category.getId();
+        String imageUrl = account.getId() + "/category/" + category.getId();
         category.setCategoryImageUrl(imageUrl);
         categoryRepository.save(category);
 
+        File folder = new File(filePath+account.getId() + "/category");
+        log.info(folder.mkdirs() ? "success make dir" : "fail make dir");
+
         File file = new File(filePath+imageUrl);
-        file.createNewFile();
+        log.info(filePath+imageUrl);
+        log.info(file.createNewFile() ? "success make file" : "fail make file");
         FileOutputStream fos = new FileOutputStream(file);
         fos.write(image.getBytes());
         fos.close();
@@ -57,7 +62,7 @@ public class CategoryService {
     // 카테고리 조회
     @Transactional
     public CategoriesResponse getCategoryList() {
-        List<Category> categories;
+        List<Category> categories = null;
         categories = categoryRepository.findAll();
         return new CategoriesResponse(categories);
     }
@@ -74,14 +79,28 @@ public class CategoryService {
     public CategoryResponse updateCategory(Long id, CategoryUpdateRequest categoryUpdateRequest) {
         Category category = findById(id);
         Category requestCategory = categoryUpdateRequest.toCategory();
-
         Category updatedCategory = category.update(requestCategory);
+
+//        Account account,MultipartFile image, throws Exception
+//        String imageUrl = account.getId() + "/category/" + category.getId();
+//        category.setCategoryImageUrl(imageUrl);
+//        categoryRepository.save(category);
+//
+//        File folder = new File(filePath+account.getId() + "/category");
+//        log.info(folder.mkdirs() ? "success make dir" : "fail make dir");
+//
+//        File file = new File(filePath+imageUrl);
+//        log.info(filePath+imageUrl);
+//        log.info(file.createNewFile() ? "success make file" : "fail make file");
+//        FileOutputStream fos = new FileOutputStream(file);
+//        fos.write(image.getBytes());
+//        fos.close();
+
         return CategoryResponse.of(updatedCategory);
     }
 
-    public Category findById(Long id) {
-        return categoryRepository.findById(id)
-                .orElseThrow();
+    private Category findById(Long id) {
+        return categoryRepository.findById(id).orElseThrow();
     }
 
     // 카테고리 삭제
