4aeac6b5a6a745948822444ab00e61f0eb2c25e4
LEEAYOUNG <souk0711@gmail.com>
2021-03-26 01:57:16 +0900

    feat / back / 나만의 이음, 상황별 이음 삭제 기능 개발 / S04P23B104-100

---
+++ backend/src/main/java/com/ssafy/eeum/category/repository/CategoryCardRepository.java
@@ -0,0 +1,10 @@
+package com.ssafy.eeum.category.repository;
+
+import com.ssafy.eeum.category.domain.CategoryCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryCardRepository extends JpaRepository<CategoryCard,Long> {
+    List<CategoryCard> findByCategoryId(Long id);
+}
