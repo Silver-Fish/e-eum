6785912992fab8358b2e0b39aa48f3351a41ad4e
LEEAYOUNG <souk0711@gmail.com>
2021-03-25 15:29:33 +0900

    feat / back / 개별 조회 Controller 개발 / S04P23B104-44

--- backend/src/main/java/com/ssafy/eeum/card/controller/CardController.java
+++ backend/src/main/java/com/ssafy/eeum/card/controller/CardController.java
@@ -1,7 +1,9 @@
 package com.ssafy.eeum.card.controller;
 
+import com.ssafy.eeum.account.domain.Account;
 import com.ssafy.eeum.card.dto.response.CardResponse;
 import com.ssafy.eeum.card.service.CardService;
+import com.ssafy.eeum.common.annotation.CurrentAccount;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotNull;
 import java.io.IOException;
 import java.util.List;
 
@@ -35,20 +38,25 @@ public class CardController {
 
     @PostMapping
     @ApiOperation(value = "카드 등록", notes = "타입(own, category, qr), 이미지, 단어를 받아서 카드를 등록한다", response = ResponseEntity.class)
-    public ResponseEntity<Long> saveCard(Authentication authentication, @RequestParam(value = "type") String type, @RequestParam(value = "typeId", required = false) String typeId, @RequestParam(value = "word") String word, @RequestParam(value = "file", required = false) MultipartFile image) {
+    public ResponseEntity<Long> saveCard(@CurrentAccount Account account, @RequestParam(value = "type") String type, @RequestParam(value = "typeId", required = false) Long typeId, @RequestParam(value = "word") String word, @RequestParam(value = "file", required = false) MultipartFile image) throws Exception{
         Long cardNo = null;
-        try {
-            cardNo = cardService.save((String) authentication.getPrincipal(), type, typeId, word, image);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+        cardNo = cardService.save(account, type, typeId, word, image);
         return ResponseEntity.ok().body(cardNo);
+    }
+
+    @GetMapping()
+    @ApiOperation(value = "개별 카드 리스트 조회", notes = "카드 id를 받아 카드리스트를 조회한다.", response = ResponseEntity.class)
+    public ResponseEntity<CardResponse> getCard(@RequestParam @NotNull Long id) {
+        CardResponse cardResponse = cardService.find(id);
+        return ResponseEntity.ok().body(cardResponse);
     }
 
     @GetMapping("/{type}")
     @ApiOperation(value = "카드 리스트 조회", notes = "타입(own, category, qr)과 타입의 번호(ex.category_id)를 받아 카드리스트를 조회. own의 경우 타입번호 미입력", response = ResponseEntity.class)
-    public ResponseEntity<List<CardResponse>> getCardList(Authentication authentication, @PathVariable String type, @RequestParam(required = false) String typeId) {
-        List<CardResponse> cardResponses = cardService.findList((String)authentication.getPrincipal(),type, typeId);
+    public ResponseEntity<List<CardResponse>> getCardList(@CurrentAccount Account account, @PathVariable String type, @RequestParam(required = false) Long typeId) {
+        List<CardResponse> cardResponses = cardService.findList(account,type, typeId);
         return ResponseEntity.ok().body(cardResponses);
     }
+
+
 }
