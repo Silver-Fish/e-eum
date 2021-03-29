8f4b077cfc62e40b89d7a68e1461ca6f77553a6c
AgFe <dbsdmscjf304@naver.com>
2021-03-29 19:06:09 +0900

    feat/front/QrListCR/S04P23B104-110,111

--- frontend/src/pages/Qr/index.js
+++ frontend/src/pages/Qr/index.js
@@ -1,23 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './index.module.css'
 import HearderComp from '../../components/HeaderComp/HeaderComp'
 import QrList from '../../components/Qr/QrList'
 import QrRegister from '../../components/Qr/QrRegister'
 import QrEdit from '../../components/Qr/QrEdit'
-
+import axios from 'axios';
 
 
 const Qr = () => {
-  // const [qrs, setQrs] = useState([
+  const token = sessionStorage.getItem('jwt');
+  const [qrs,setQrs] = useState([]);
+  // const qrs = useState([
   //   ['스타벅스'], ['롯데리아'],['다이소'], ['편의점'], ['K치과']
-  // ])
-  const qrs = useState([
-    ['스타벅스'], ['롯데리아'],['다이소'], ['편의점'], ['K치과']
-  ])[0]
+  // ])[0]
   const [isQrResister, setQrResister] = useState(false)
   const [isQrEdit, setQrEdit] = useState(false)
   const [slectedQrName, setSlectedQrName] = useState('')
 
+  const data = useState([
+  ['스타벅스'], ['롯데리아'],['다이소'], ['편의점'], ['K치과']
+  ])[0]
+  useEffect(()=>{
+    setQrs(data);
+    // axios
+    // .get(process.env.REACT_APP_API_URL+'/QrList',{
+    //   headers:{
+    //     Authorization:token,
+    //   },
+    // })
+    // .then((res)=>{
+    //   if(res.status===200){
+    //     setQrs(res.data);
+    //   }
+    // })
+    // .catch((err)=>{
+    //   console.log("Qr리스트 받아오기 실패 ㅠㅠ");
+    //   console.log(err);
+    // })
+  })
 
   const changeQrResisterState = () => {
     setQrResister(!isQrResister)
