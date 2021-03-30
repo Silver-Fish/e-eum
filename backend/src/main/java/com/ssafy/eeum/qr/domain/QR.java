b340ccb5876dea79714ff51967afae15fbb89ebc
차수연 <pepprbell@gmail.com>
2021-03-30 18:19:41 +0900

    feat / back / dto 수정 / S04P23B104-53

--- backend/src/main/java/com/ssafy/eeum/qr/domain/QR.java
+++ backend/src/main/java/com/ssafy/eeum/qr/domain/QR.java
@@ -1,5 +1,6 @@
 package com.ssafy.eeum.qr.domain;
 
+import com.ssafy.eeum.account.domain.Account;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,8 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,6 +24,10 @@ public class QR {
     @Column(name="qr_id")
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Account account;
+
     private String title;
 
     @Lob
@@ -32,6 +39,9 @@ public class QR {
     @UpdateTimestamp
     private LocalDateTime updateDate;
 
+    @OneToMany(mappedBy = "account", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<AccountQr> accountQrs = new ArrayList<>();
+
     @Builder
     public QR(String title, String qrUrl) {
         this.title = title;
@@ -40,5 +50,13 @@ public class QR {
 
     public void setQrUrl(String qrUrl) {
         this.qrUrl = qrUrl;
+    }
+
+    public void update(QR requestQr) {
+        this.title = requestQr.title;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
     }
 }
---
+++ backend/src/main/java/com/ssafy/eeum/qr/dto/response/QrResponse.java
@@ -0,0 +1,35 @@
+package com.ssafy.eeum.qr.dto.response;
+
+import com.ssafy.eeum.account.domain.Account;
+import com.ssafy.eeum.qr.domain.QR;
+import com.ssafy.eeum.qr.repository.QrRepository;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class QrResponse {
+    private Long id;
+    private String title;
+    private String qr_url;
+
+    public QrResponse(Long id, String title, String qr_url) {
+        this.id = id;
+        this.title = title;
+        this.qr_url = qr_url;
+    }
+
+    public static QrResponse of(QR qr) {
+        return new QrResponse(qr.getId(),qr.getTitle(),qr.getQrUrl());
+    }
+
+    public static List<QrResponse> listof(List<QR> qrs) {
+        List<QrResponse> qrResponses = new ArrayList<>();
+
+        for (QR qr : qrs) {
+            qrResponses.add(of(qr));
+        }
+        return qrResponses;
+    }
+}
