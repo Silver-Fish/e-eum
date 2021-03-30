f7b38e0776282065b8c752ff127b46737b66661d
juheegg <eespacio95@gmail.com>
2021-03-31 07:51:14 +0900

    feat / back / swagger에 jwt 인증 기능 추가

--- backend/src/main/java/com/ssafy/eeum/common/config/SwaggerConfig.java
+++ backend/src/main/java/com/ssafy/eeum/common/config/SwaggerConfig.java
@@ -1,15 +1,22 @@
 package com.ssafy.eeum.common.config;
 
 import com.google.common.base.Predicates;
+import com.ssafy.eeum.common.security.JwtProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -28,6 +35,23 @@ public class SwaggerConfig {
                 .select()
                 .apis(Predicates.not(RequestHandlerSelectors.
                         basePackage("org.springframework.boot")))
-                .paths(PathSelectors.any()).build();
+                .paths(PathSelectors.any()).build()
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", JwtProperties.HEADER_STRING, "header");
+    }
+
+    private springfox.documentation.spi.service.contexts.SecurityContext securityContext() {
+        return springfox.documentation.spi.service.contexts.SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 }
