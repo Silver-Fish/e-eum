4d0c2a24dfcd2767a8c10f866b5d5aefe5c9d9d3
AgFe <dbsdmscjf304@naver.com>
2021-04-06 23:42:07 +0900

    fix/frornt/CookieTimeSetting

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,23 +1,25 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import axios from 'axios';
-import HeaderComp from '../../components/HeaderComp/HeaderComp';
-import LabelComp from '../../components/LabelComp/LabelComp';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import HeaderComp from "../../components/HeaderComp/HeaderComp";
+import LabelComp from "../../components/LabelComp/LabelComp";
 
-import InputComp from '../../components/InputComp/InputComp';
-import styles from './index.module.css';
-import { useCookies } from 'react-cookie';
+import InputComp from "../../components/InputComp/InputComp";
+import styles from "./index.module.css";
+import { useCookies } from "react-cookie";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [autoLogin, setAutoLogin] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [autoLogin, setAutoLogin] = useState("");
   const history = useHistory();
 
-  const [cookies, setCookie] = useCookies(['cookie']); // eslint-disable-line no-unused-vars
+  const [cookies, setCookie] = useCookies(["cookie"]); // eslint-disable-line no-unused-vars
 
   const setCookieFunc = () => {
-    setCookie('cookie', sessionStorage.getItem('jwt'), { maxAge: 2000 });
+    setCookie("cookie", sessionStorage.getItem("jwt"), {
+      maxAge: 60 * 60 * 24 * 365,
+    });
   };
 
   const onEmailHandler = (e) => {
@@ -39,24 +41,24 @@ const Login = () => {
       password: password,
     };
     axios
-      .post(process.env.REACT_APP_API_URL + '/login', userData)
+      .post(process.env.REACT_APP_API_URL + "/login", userData)
 
       .then((res) => {
         if (res.status === 200) {
           // localStorage.setItem('email', res.data.data.email);
           // localStorage.setItem('name', res.data.data.name);
-          sessionStorage.setItem('jwt', res.headers.authorization);
+          sessionStorage.setItem("jwt", res.headers.authorization);
           if (autoLogin) {
             setCookieFunc();
           }
-          history.push('/');
+          history.push("/");
         } else {
-          alert('ID와 PW가 일치하지 않습니다.^0^');
+          alert("ID와 PW가 일치하지 않습니다.^0^");
         }
       })
       .catch((err) => {
         console.log(err);
-        alert('다시해^0^');
+        alert("다시해^0^");
       });
   };
 
@@ -65,8 +67,16 @@ const Login = () => {
       <HeaderComp headertitle="로그인" />
 
       <form className={styles.MainForm} onSubmit={onSubmitHandler}>
-        <InputComp type="email" placeholder="이메일" InputChange={onEmailHandler} />
-        <InputComp type="password" placeholder="비밀번호" InputChange={onPasswordHandler} />
+        <InputComp
+          type="email"
+          placeholder="이메일"
+          InputChange={onEmailHandler}
+        />
+        <InputComp
+          type="password"
+          placeholder="비밀번호"
+          InputChange={onPasswordHandler}
+        />
 
         <input
           className={styles.input_check}
@@ -79,7 +89,10 @@ const Login = () => {
         <label htmlFor="check">자동로그인</label>
         <div className={styles.labelForm}>
           <LabelComp textValue="회원가입" handleClickPath="./userRegister" />
-          <LabelComp textValue="비밀번호 찾기" handleClickPath="./findPassword" />
+          <LabelComp
+            textValue="비밀번호 찾기"
+            handleClickPath="./findPassword"
+          />
         </div>
         <button className={styles.login_button} type="submit">
           로그인
