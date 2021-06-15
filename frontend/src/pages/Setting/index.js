398071012f9058f490adab38356b16efe518cc10
shindonghyun <aclab.shin@gmail.com>
2021-06-15 16:04:15 +0900

    [refactor]: refactoring

--- frontend/src/pages/Setting/index.js
+++ frontend/src/pages/Setting/index.js
@@ -9,18 +9,13 @@ const Setting = () => {
             <div className={styles.setting_text}>
                 <h2>
                     <span  className={styles.red}>음성속도 </span> 
-                        조절 및 
-                    <span  className={styles.red}> 음성합성 </span> 
-                        서비스 준비중 입니다.
+                        조절 서비스 준비중 입니다.
                 </h2>
 
                 
                 <h2  className={styles.red}>Comming Soon</h2>
                 <h3 className={styles.red}>음성속도</h3>
-                  ~2021-05-05
-                <h3 className={styles.red}>음성합성</h3>
-                ~2021-05-17
-
+                  ~2022
             </div>
         </div>
     )
